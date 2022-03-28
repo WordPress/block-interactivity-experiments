446e1920c2f6b43ba51913712086828741700cef
Luis Herranz <luisherranz@gmail.com>
2022-03-28 18:03:09 +0200

    Remove console.log

--- src/view.js
+++ src/view.js
@@ -13,21 +13,20 @@ const Block = ({ attributes, children }) => (
 );
 
 window.addEventListener("load", (event) => {
-  document.querySelectorAll("gutenberg-root").forEach((block) => {
-    console.log(block);
-  });
-  document.querySelectorAll("gutenberg-root").forEach((block) => {
-    const attributes = JSON.parse(block.dataset["gutenbergAttributes"]);
-    const innerBlocks = block.querySelector("gutenberg-inner-blocks");
-    const el = createElement(
-      Block,
-      { attributes, suppressHydrationWarning: true },
-      innerBlocks !== null &&
-        createElement(Children, {
-          value: innerBlocks.innerHTML,
-          suppressHydrationWarning: true,
-        })
-    );
-    hydrate(el, block);
-  });
+  document
+    .querySelectorAll(".wp-block-luisherranz-block-hydration-experiments")
+    .forEach((block) => {
+      const attributes = JSON.parse(block.dataset["gutenbergAttributes"]);
+      const innerBlocks = block.querySelector("gutenberg-inner-blocks");
+      const el = createElement(
+        Block,
+        { attributes, suppressHydrationWarning: true },
+        innerBlocks !== null &&
+          createElement(Children, {
+            value: innerBlocks.innerHTML,
+            suppressHydrationWarning: true,
+          })
+      );
+      hydrate(el, block);
+    });
 });
