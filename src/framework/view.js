f80519d6c08d837d1c7229f1d07dc7b6344e9d9a
Luis Herranz <luisherranz@gmail.com>
2022-03-29 15:55:44 +0200

    Small refactor

--- src/framework/view.js
+++ src/framework/view.js
@@ -11,21 +11,19 @@ const Children = ({ value }) => {
 Children.shouldComponentUpdate = () => false;
 
 const view = (Comp) => {
-  window.addEventListener("load", (event) => {
+  window.addEventListener("load", () => {
     document
       .querySelectorAll(".wp-block-luisherranz-block-hydration-experiments")
       .forEach((block) => {
         const attributes = JSON.parse(block.dataset["gutenbergAttributes"]);
         const innerBlocks = block.querySelector("gutenberg-inner-blocks");
-        debugger;
         const el = createElement(
           Comp,
           { attributes, suppressHydrationWarning: true },
-          innerBlocks !== null &&
-            createElement(Children, {
-              value: innerBlocks.innerHTML,
-              suppressHydrationWarning: true,
-            })
+          createElement(Children, {
+            value: innerBlocks.innerHTML,
+            suppressHydrationWarning: true,
+          })
         );
         hydrate(el, block);
       });
