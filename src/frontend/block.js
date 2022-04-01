fc8e0c2f0c2f760e738251ad3f6219aa27539138
Luis Herranz <luisherranz@gmail.com>
2022-04-01 12:30:04 +0200

    Switch to hide/show interactivity

--- src/frontend/block.js
+++ src/frontend/block.js
@@ -1,12 +1,19 @@
+import { useState } from "@wordpress/element";
 import Title from "./title";
 import Button from "./button";
 
-const Block = ({ attributes, children }) => (
-  <>
-    <Title message={attributes.message} />
-    <Button />
-    {children}
-  </>
-);
+const Block = ({ attributes, children, isView }) => {
+  const [show, setShow] = isView ? useState(true) : [true];
+  const [counter, setCounter] = isView ? useState(0) : [0];
+
+  return (
+    <>
+      <Title message={attributes.message} />
+      <Button handler={() => setShow(!show)} />
+      <button onClick={() => setCounter(counter + 1)}>{counter}</button>
+      {show && children}
+    </>
+  );
+};
 
 export default Block;
