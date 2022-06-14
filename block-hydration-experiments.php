d6da90939824c9279aa8c4883561dee62247cc95
Bernie Reiter <ockham@raz.or.at>
2022-06-14 21:40:36 +0200

    Add interactive block attributes during server-side rendering

--- block-hydration-experiments.php
+++ block-hydration-experiments.php
@@ -14,3 +14,38 @@ function block_hydration_experiments_init() {
 	register_block_type( plugin_dir_path( __FILE__ ) . 'build/blocks/block-hydration-experiments-parent/' );
 }
 add_action( 'init', 'block_hydration_experiments_init' );
+
+function bhe_block_wrapper( $block_content, $block, $instance ) {
+	// We might want to use a flag from block.json as the criterion here.
+	if ( ! in_array(
+		$block['blockName'],
+		array(
+			'bhe/block-hydration-experiments-parent',
+			'bhe/block-hydration-experiments-child'
+		),
+		true
+	) ) {
+		return $block_content;
+	}
+
+	$block_wrapper = sprintf(
+		'<gutenberg-interactive-block ' .
+		'data-gutenberg-block-type="%1$s" ' .
+		'data-gutenberg-context-used="%2$s" ' .
+		'data-gutenberg-context-provided="%3$s" ' .
+		'data-gutenberg-attributes="%4$s" ' .
+		'data-gutenberg-hydrate="idle">',
+		esc_attr( $block['blockName'] ),
+		esc_attr( json_encode( $instance->block_type->uses_context ) ),
+		esc_attr( json_encode( $instance->block_type->provides_context ) ),
+		esc_attr( json_encode( $block['attrs'] ) ),
+	) . '%1$s</gutenberg-interactive-block>';
+
+	$template_wrapper = '<template class="gutenberg-inner-blocks">%1$s</template>';
+
+	$empty_template = sprintf( $template_wrapper, '' );
+	$template       = sprintf( $template_wrapper, sprintf( $block_wrapper, $block_content . $empty_template ) );
+	return sprintf( $block_wrapper, $block_content . $template );
+}
+
+add_filter( 'render_block', 'bhe_block_wrapper', 10, 3 );
