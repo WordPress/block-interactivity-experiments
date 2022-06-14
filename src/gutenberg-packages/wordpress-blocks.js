9aa1245788c23ec58050d35bb08e1cc8e4a44dc1
Bernie Reiter <ockham@raz.or.at>
2022-06-14 22:00:22 +0200

    Remove gutenberg-interactive-block wrapper from save()

--- src/gutenberg-packages/wordpress-blocks.js
+++ src/gutenberg-packages/wordpress-blocks.js
@@ -1,5 +1,6 @@
 import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
 import { registerBlockType as gutenbergRegisterBlockType } from '@wordpress/blocks';
+import { Fragment } from '@wordpress/element';
 import {
 	getBlockContext,
 	getFrontendAttributes,
@@ -15,17 +16,7 @@ const save = ( name, Comp ) =>
 		const innerBlocksProps = useInnerBlocksProps.save();
 
 		return (
-			<gutenberg-interactive-block
-				data-gutenberg-block-type={name}
-				data-gutenberg-context-used={JSON.stringify( usesContext )}
-				data-gutenberg-context-provided={JSON.stringify( providesContext )}
-				data-gutenberg-attributes={JSON.stringify( frontendAttributes )}
-				data-gutenberg-sourced-attributes={JSON.stringify(
-					sourcedFrontendAttributes,
-				)}
-				data-gutenberg-block-props={JSON.stringify( blockProps )}
-				data-gutenberg-hydrate='idle'
-			>
+			<Fragment>
 				<Comp blockProps={blockProps} attributes={attributes}>
 					<gutenberg-inner-blocks {...innerBlocksProps} />
 				</Comp>
@@ -34,7 +25,7 @@ const save = ( name, Comp ) =>
             if Comp doesn't render them. */
 				}
 				<template class='gutenberg-inner-blocks' {...innerBlocksProps} />
-			</gutenberg-interactive-block>
+			</Fragment>
 		);
 	};
 
