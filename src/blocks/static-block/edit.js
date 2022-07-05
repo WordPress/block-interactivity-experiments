1fcf22a3f7376e7e3ba402518ea5c994f8bd580d
Michal Czaplinski <mmczaplinski@gmail.com>
2022-07-05 18:49:49 -0500

    Add RichText fields to change context in editor

--- src/blocks/static-block/edit.js
+++ src/blocks/static-block/edit.js
@@ -1,10 +1,22 @@
 import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
+import { RichText } from '../../gutenberg-packages/wordpress-blockeditor';
 
-const Edit = ( { attributes } ) => {
+const Edit = ( { attributes, setAttributes } ) => {
 	const blockProps = useBlockProps();
 	return (
 		<div {...blockProps}>
-			<p>static block</p>
+			<div>
+				We can use the "content" field below to change the value of the context
+			</div>
+			<RichText
+				tagName='h3'
+				className='content'
+				onChange={( val ) => setAttributes( { content: val } )}
+				placeholder='Enter the content'
+				value={attributes.content}
+			>
+				{attributes.content}
+			</RichText>
 			<InnerBlocks />
 		</div>
 	);
