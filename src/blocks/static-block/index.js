50a861142f42a0d7e9228df679ca180fddb2e72b
Michal Czaplinski <mmczaplinski@gmail.com>
2022-07-12 16:10:19 -0500

    Import style.css in index.js of static-block

--- src/blocks/static-block/index.js
+++ src/blocks/static-block/index.js
@@ -2,6 +2,7 @@ import { registerBlockType } from '../../gutenberg-packages/wordpress-blocks';
 import metadata from './block.json';
 import edit from './edit';
 import save from './save';
+import './style.scss';
 
 const { name } = metadata;
 
