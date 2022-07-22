ee06632cf14baa6a7e159f026e8f66f8f1b060d4
Luis Herranz <luisherranz@gmail.com>
2022-07-22 17:48:21 +0200

    Prettify everything

--- src/gutenberg-packages/react-context.js
+++ src/gutenberg-packages/react-context.js
@@ -1,66 +1,67 @@
 import { createGlobal } from './utils';
 import { useEffect, useState } from './wordpress-element';
 
-const subscribers = createGlobal( 'reactContextSubscribers', new WeakMap() );
-const values = createGlobal( 'reactContextValues', new WeakMap() );
+const subscribers = createGlobal('reactContextSubscribers', new WeakMap());
+const values = createGlobal('reactContextValues', new WeakMap());
 
-const updateValue = ( { element, context, value } ) => {
-	if ( !values.has( element ) ) {
-		values.set( element, new WeakMap() );
+const updateValue = ({ element, context, value }) => {
+	if (!values.has(element)) {
+		values.set(element, new WeakMap());
 	}
-	values.get( element ).set( context, value );
+	values.get(element).set(context, value);
 };
 
-const getValue = ( { element, context } ) => {
-	if ( values.has( element ) ) {
-		return values.get( element ).get( context );
+const getValue = ({ element, context }) => {
+	if (values.has(element)) {
+		return values.get(element).get(context);
 	}
 };
 
-const subscribe = ( { element, context, callback } ) => {
-	if ( !subscribers.has( element ) ) {
-		subscribers.set( element, new WeakMap() );
+const subscribe = ({ element, context, callback }) => {
+	if (!subscribers.has(element)) {
+		subscribers.set(element, new WeakMap());
 	}
-	if ( !subscribers.get( element ).has( context ) ) {
-		subscribers.get( element ).set( context, new Set() );
+	if (!subscribers.get(element).has(context)) {
+		subscribers.get(element).set(context, new Set());
 	}
-	subscribers.get( element ).get( context ).add( callback );
+	subscribers.get(element).get(context).add(callback);
 };
 
-const unsubscribe = ( { element, context, callback } ) => {
-	subscribers.get( element ).get( context ).delete( callback );
+const unsubscribe = ({ element, context, callback }) => {
+	subscribers.get(element).get(context).delete(callback);
 };
 
-const updateProvider = ( { element, context, value } ) => {
-	updateValue( { element, context, value } );
+const updateProvider = ({ element, context, value }) => {
+	updateValue({ element, context, value });
 
-	if (subscribers.has( element ) && subscribers.get( element ).has( context )) {
+	if (subscribers.has(element) && subscribers.get(element).has(context)) {
 		// This setTimeout prevents a React warning about calling setState inside a
 		// render() function, which is misleading because the render() function
 		// belongs to a different React application. It may not happen in React 18.
-		setTimeout( () => {
+		setTimeout(() => {
 			subscribers
-				.get( element )
-				.get( context )
-				.forEach(callback => callback( value ));
-		} );
+				.get(element)
+				.get(context)
+				.forEach((callback) => callback(value));
+		});
 	}
 };
 
-export const createProvider = ( { element, context } ) =>
-	( { children } ) => {
-		const [ value, setValue ] = useState( getValue( { element, context } ) );
+export const createProvider =
+	({ element, context }) =>
+	({ children }) => {
+		const [value, setValue] = useState(getValue({ element, context }));
 
-		useEffect( () => {
-			subscribe( { element, context, callback: setValue } );
-			return () => unsubscribe( { element, context, callback: setValue } );
-		}, [] );
+		useEffect(() => {
+			subscribe({ element, context, callback: setValue });
+			return () => unsubscribe({ element, context, callback: setValue });
+		}, []);
 
 		return <context.Provider value={value}>{children}</context.Provider>;
 	};
 
-export const Consumer = ( { element, context } ) => (
+export const Consumer = ({ element, context }) => (
 	<context.Consumer>
-		{value => updateProvider( { element, context, value } )}
+		{(value) => updateProvider({ element, context, value })}
 	</context.Consumer>
 );
--- src/gutenberg-packages/utils.js
+++ src/gutenberg-packages/utils.js
@@ -1,19 +1,19 @@
 import { text } from 'hpq';
 
 // See https://github.com/WordPress/gutenberg/blob/trunk/packages/blocks/src/api/parser/get-block-attributes.js#L185
-export const matcherFromSource = ( sourceConfig ) => {
-	switch ( sourceConfig.source ) {
+export const matcherFromSource = (sourceConfig) => {
+	switch (sourceConfig.source) {
 		// TODO: Add cases for other source types.
 		case 'text':
-			return text( sourceConfig.selector );
+			return text(sourceConfig.selector);
 	}
 };
 
 // We have to do this because of the way we are currently bundling the code in
 // this repo, each block gets its own copy of this file, but this won't happen
 // if/when we do this in Gutenberg.
-export const createGlobal = ( name, initialValue ) => {
-	if ( typeof window[name] === 'undefined' ) {
+export const createGlobal = (name, initialValue) => {
+	if (typeof window[name] === 'undefined') {
 		window[name] = initialValue;
 	}
 	return window[name];
