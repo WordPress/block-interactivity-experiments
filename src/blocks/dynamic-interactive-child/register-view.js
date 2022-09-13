003f6681f7b5530fca96d9b6ea59988ad80c3d37
Mario Santos <santosguillamot@gmail.com>
2022-09-13 15:43:06 +0200

    Remove timeouts in register-view.js files

--- src/blocks/dynamic-interactive-child/register-view.js
+++ src/blocks/dynamic-interactive-child/register-view.js
@@ -3,8 +3,6 @@ import ThemeContext from '../../context/theme';
 import registerBlockView from '../../gutenberg-packages/register-block-view';
 import View from './view';
 
-setTimeout(() => {
-    registerBlockView('bhe/dynamic-interactive-child', View, {
-        usesContext: [ThemeContext, CounterContext],
-    });
-}, 2000);
+registerBlockView('bhe/dynamic-interactive-child', View, {
+    usesContext: [ThemeContext, CounterContext],
+});
--- src/blocks/dynamic-interactive-parent/register-view.js
+++ src/blocks/dynamic-interactive-parent/register-view.js
@@ -3,8 +3,6 @@ import ThemeContext from '../../context/theme';
 import registerBlockView from '../../gutenberg-packages/register-block-view';
 import View from './view';
 
-setTimeout(() => {
-    registerBlockView('bhe/dynamic-interactive-parent', View, {
-        providesContext: [ThemeContext, CounterContext],
-    });
-}, 1000);
+registerBlockView('bhe/dynamic-interactive-parent', View, {
+    providesContext: [ThemeContext, CounterContext],
+});
