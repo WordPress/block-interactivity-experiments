23c14829efca8bcfc1d293d90550beb5f606c269
Luis Herranz <luisherranz@gmail.com>
2022-09-29 10:55:54 +0200

    Rename blockProvidesBlockContext directive file

--- src/gutenberg-packages/hydration.js
+++ src/gutenberg-packages/hydration.js
@@ -2,7 +2,7 @@ import 'preact/debug';
 import { hydrate } from 'preact';
 import toVdom from './to-vdom';
 import '../directives/wp-block-type';
-import '../directives/wp-block-provides-context';
+import '../directives/wp-block-provides-block-context';
 import '../directives/wp-log';
 
 const dom = document.querySelector('.wp-site-blocks');
