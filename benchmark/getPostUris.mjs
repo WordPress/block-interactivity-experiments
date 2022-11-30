bbe37fe251c5d1ce1524f6513c3f89fd0cf4d1f4
Carlos Bravo <carlos.bravo@automattic.com>
2022-11-30 14:16:10 +0100

    Initial draft of script getting posts using Rest API

---
+++ benchmark/getPostUris.mjs
@@ -0,0 +1,109 @@
+import * as fs from 'fs';
+import { parse } from 'csv-parse';
+import minimist from 'minimist';
+import { request } from '@playwright/test';
+
+// Get the CLI arguments for the file to use
+// and the number of pages to test concurrently.
+const { _, concurrency } = minimist(process.argv.slice(2), {
+	default: { concurrency: 10 },
+});
+const fileArg = _[0];
+if (typeof fileArg === 'undefined') {
+	console.error(
+		'\x1b[41m',
+		'\nPlease provide a CSV file as an argument\n\nFor example: npm run benchmark 1.csv\n'
+	);
+	process.exit(1);
+}
+
+// Run the benchmark
+(async () => {
+	try {
+		const domains = await getDomains();
+		// (B) WRITE TO FILE
+		await fs.writeFileSync('./benchmark/data/posts.csv', '');
+
+		await asyncParallelQueue(concurrency || 40, domains, async (url) => {
+			return await testUrl(url);
+		});
+	} catch (e) {
+		console.log(e);
+	}
+})();
+
+async function testUrl(url) {
+	try {
+		const context = await request.newContext({
+			baseURL: `https://${url}`,
+		});
+
+		try {
+			const response = await context.get(
+				'/wp-json/wp/v2/posts?post_per_page=1'
+			);
+			const href = await response.json();
+			await fs.appendFileSync(
+				'./benchmark/data/posts.csv',
+				`\n${href[0].link}`
+			);
+		} catch (error) {
+			console.log(error);
+		}
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+async function getDomains() {
+	const domains = [];
+	const csvFile = new URL(`./data/${fileArg}`, import.meta.url).pathname;
+	return new Promise((resolve, reject) => {
+		fs.createReadStream(csvFile)
+			.pipe(
+				parse({
+					delimiter: ',',
+					from_line: 2,
+					skip_records_with_error: true,
+				})
+			)
+			.on('data', (row) => {
+				domains.push(row[0]);
+			})
+			.on('end', () => {
+				console.log('Domains Created');
+				resolve(domains);
+			})
+			.on('error', (error) => {
+				reject(error);
+			});
+	});
+}
+
+async function asyncParallelQueue(concurrency, items, func) {
+	const batch = items.slice(0, concurrency);
+	let promisesArray = batch
+		.map(func)
+		.map((p, i) => [i, p.then(() => i).catch(() => i)]);
+
+	// Create a "pool" of Promises.
+	const pool = new Map(promisesArray);
+
+	for (let index = 0; index < items.length; index++) {
+		const key = await Promise.race(pool.values());
+		pool.delete(key);
+		if (concurrency + index < items.length) {
+			pool.set(
+				concurrency + index,
+				func(items[concurrency + index])
+					.then(() => concurrency + index)
+					.catch(() => concurrency + index)
+			);
+		}
+	}
+}
+
+process.on('SIGINT', () => {
+	console.log('Caught interrupt signal');
+	process.exit(1);
+});
