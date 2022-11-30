0ccf1da19b71364e7ca2ac9040d9e0cfc91e4ece
Michal Czaplinski <mmczaplinski@gmail.com>
2022-11-29 21:17:26 -0500

    Add a new config to build the test file

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,95 @@
 const { resolve } = require('path');
 
-module.exports = {
-	entry: {
-		runtime: './src/runtime',
-	},
-	output: {
-		filename: '[name].js',
-		path: resolve(process.cwd(), 'build'),
-	},
-	optimization: {
-		runtimeChunk: {
-			name: 'vendors',
+module.exports = [
+	{
+		entry: {
+			runtime: './src/runtime',
 		},
-		splitChunks: {
-			cacheGroups: {
-				vendors: {
-					test: /[\\/]node_modules[\\/]/,
-					name: 'vendors',
-					minSize: 0,
-					chunks: 'all',
+		output: {
+			filename: '[name].js',
+			path: resolve(process.cwd(), 'build'),
+		},
+		optimization: {
+			runtimeChunk: {
+				name: 'vendors',
+			},
+			splitChunks: {
+				cacheGroups: {
+					vendors: {
+						test: /[\\/]node_modules[\\/]/,
+						name: 'vendors',
+						minSize: 0,
+						chunks: 'all',
+					},
 				},
 			},
 		},
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(j|t)sx?$/,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: require.resolve('babel-loader'),
-						options: {
-							cacheDirectory:
-								process.env.BABEL_CACHE_DIRECTORY || true,
-							babelrc: false,
-							configFile: false,
-							presets: [
-								[
-									'@babel/preset-react',
-									{
-										runtime: 'automatic',
-										importSource: 'preact',
-									},
+		module: {
+			rules: [
+				{
+					test: /\.(j|t)sx?$/,
+					exclude: /node_modules/,
+					use: [
+						{
+							loader: require.resolve('babel-loader'),
+							options: {
+								cacheDirectory:
+									process.env.BABEL_CACHE_DIRECTORY || true,
+								babelrc: false,
+								configFile: false,
+								presets: [
+									[
+										'@babel/preset-react',
+										{
+											runtime: 'automatic',
+											importSource: 'preact',
+										},
+									],
 								],
-							],
+							},
 						},
-					},
-				],
-			},
-		],
+					],
+				},
+			],
+		},
 	},
-};
+	{
+		entry: {
+			hydrationScriptForTesting: './benchmark/hydrationScript.js',
+		},
+		output: {
+			filename: '[name].js',
+			path: resolve(process.cwd(), 'build'),
+		},
+		devtool: 'inline-source-map',
+		module: {
+			rules: [
+				{
+					test: /\.(j|t)sx?$/,
+					exclude: /node_modules/,
+					use: [
+						{
+							loader: require.resolve('babel-loader'),
+							options: {
+								cacheDirectory:
+									process.env.BABEL_CACHE_DIRECTORY || true,
+								babelrc: false,
+								configFile: false,
+								presets: [
+									[
+										'@babel/preset-env',
+										{
+											targets: {
+												esmodules: true,
+											},
+										},
+									],
+								],
+							},
+						},
+					],
+				},
+			],
+		},
+	},
+];
