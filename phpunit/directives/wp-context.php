bf4d7f6f2ffe5e1d1ab58b8be0819c98c7eccb03
Bernie Reiter <ockham@raz.or.at>
2022-12-19 18:14:13 +0100

    Minor cleanup

--- phpunit/directives/wp-context.php
+++ phpunit/directives/wp-context.php
@@ -8,11 +8,11 @@ require_once __DIR__ . '/../../src/directives/wp-context.php';
 /**
  * Tests for the wp-context directive.
  *
- * @group directives
+ * @group  directives
+ * @covers process_wp_context
  */
 class Tests_Directives_WpContext extends WP_UnitTestCase {
 	public function test_directive_updates_context() {
-		$attributes    = array();
 		$content = <<<EOF
 			<wp-show when="context.myblock.open">
 				<div>I should be shown!</div>
