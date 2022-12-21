6f4529915593200a21811937a3bd4aa3e92cfa4f
Bernie Reiter <ockham@raz.or.at>
2022-12-21 15:57:24 +0100

    Introduce WP_Directive_Context helper class

--- phpunit/directives/wp-context.php
+++ phpunit/directives/wp-context.php
@@ -15,8 +15,7 @@ require_once __DIR__ . '/../../src/html/index.php';
  */
 class Tests_Directives_WpContext extends WP_UnitTestCase {
 	public function test_directive_updates_context() {
-		$context = array( 'myblock' => array( 'open' => false ) );
-		$levels = array( $context );
+		$context = new WP_Directive_Context( array( 'myblock' => array( 'open' => false ) ) );
 
 		$markup = <<<EOF
 			<wp-context data='{ "myblock": { "open": true } }'>
@@ -29,29 +28,29 @@ EOF;
 		$tags = new WP_HTML_Tag_Processor( $markup );
 		$tags->next_tag();
 
-		process_wp_context( $tags, $levels );
+		process_wp_context( $tags, $context );
 
-		$this->assertSame( array( 'myblock' => array( 'open' => true ) ), end( $levels ) );
+		$this->assertSame( array( 'myblock' => array( 'open' => true ) ), $context->get_context() );
 		// TODO: Verify that markup is unchanged?
 
 		$tags->next_tag();
-		process_wp_context( $tags, $levels );
+		process_wp_context( $tags, $context );
 
-		$this->assertSame( array( 'myblock' => array( 'open' => false ) ), end( $levels ) );
+		$this->assertSame( array( 'myblock' => array( 'open' => false ) ), $context->get_context() );
 
 		$tags->next_tag(); // <span>
-		process_wp_context( $tags, $levels );
+		process_wp_context( $tags, $context );
 
 		$tags->next_tag( array( 'tag_closers' => 'visit' ) ); //  </span>
 
 		$tags->next_tag( array( 'tag_closers' => 'visit' ) ); //  </wp-context> for wp-context
 
-		process_wp_context( $tags, $levels );
+		process_wp_context( $tags, $context );
 
 		$tags->next_tag();
 		$this->assertSame( 'context-true', $tags->get_attribute(( 'class' ) ) );
-		process_wp_context( $tags, $levels );
+		process_wp_context( $tags, $context );
 
-		$this->assertSame( array( 'myblock' => array( 'open' => true ) ), end( $levels ) );
+		$this->assertSame( array( 'myblock' => array( 'open' => true ) ), $context->get_context() );
 	}
 }
--- src/directives/wp-context.php
+++ src/directives/wp-context.php
@@ -1,14 +1,32 @@
 <?php
 
-// TODO: I think we need to clear context once we encounter the closing tag.
-function process_wp_context( &$tags, &$levels ) {
+class WP_Directive_Context {
+	protected $stack = array( array() );
+
+    function __construct( $context ) {
+		$this->set_context( $context );
+    }
+
+	public function get_context() {
+		return end( $this->stack );
+	}
+
+	public function set_context( $context ) {
+		array_push( $this->stack, array_replace_recursive( $this->get_context(), $context ) );
+	}
+
+	public function rewind_context() {
+		array_pop( $this->stack );
+	}
+}
+
+function process_wp_context( &$tags, &$context ) {
 	if( $tags->is_tag_closer() && 'WP-CONTEXT' === $tags->get_tag() ) {
-		array_pop( $levels );
+		$context->rewind_context();
 	}
 
 	if ( 'WP-CONTEXT' === $tags->get_tag() ) {
 		$value = $tags->get_attribute( 'data' );
-		// ...
 	} else {
 		$value = $tags->get_attribute( 'wp-context' );
 	}
@@ -21,6 +39,5 @@ function process_wp_context( &$tags, &$levels ) {
 	$new_context = json_decode( $value, true );
 	// TODO: Error handling.
 
-	$context = end( $levels );
-	array_push( $levels, array_replace_recursive( $context, $new_context ) );
+	$context->set_context( $new_context );
 }
