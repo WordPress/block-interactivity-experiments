fde1f0ea272784b0eb7c34b874a24c126c277de0
Bernie Reiter <ockham@raz.or.at>
2022-12-22 15:48:09 +0100

    Copy directives unit tests from #118

---
+++ phpunit/directives/wp-bind.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * wp-bind directive test.
+ */
+
+require_once __DIR__ . '/../../src/directives/wp-bind.php';
+require_once __DIR__ . '/../../src/directives/wp-context.php';
+
+require_once __DIR__ . '/../../src/html/index.php';
+
+/**
+ * Tests for the wp-bind directive.
+ *
+ * @group  directives
+ * @covers process_wp_bind
+ */
+class Tests_Directives_WpBind extends WP_UnitTestCase {
+	public function test_directive() {
+		$markup = '<img wp-bind:src="context.myblock.imageSource" />';
+		$tags = new WP_HTML_Processor( $markup );
+		$tags->next_tag();
+
+		$context_before = new WP_Directive_Context( array( 'myblock' => array( 'imageSource' => './wordpress.png' ) ) );
+		$context = $context_before;
+		process_wp_bind( $tags, $context );
+
+		$this->assertSame(
+			'<img src="./wordpress.png" wp-bind:src="context.myblock.imageSource" />',
+			$tags->get_updated_html()
+		);
+		// $this->assertSame( './wordpress.png', $tags->get_attribute( 'src' ) ); // FIXME: Broken; will be fixed by https://github.com/WordPress/gutenberg/pull/46598.
+		$this->assertSame( $context_before->get_context(), $context->get_context(), 'wp-bind directive changed context' );
+	}
+}
---
+++ phpunit/directives/wp-show.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * wp-show directive test.
+ */
+
+require_once __DIR__ . '/../../src/directives/wp-show.php';
+require_once __DIR__ . '/../../src/directives/wp-context.php';
+
+/**
+ * Tests for the wp-show directive.
+ *
+ * @group  directives
+ * @covers process_wp_show
+ */
+class Tests_Directives_WpShow extends WP_UnitTestCase {
+	public function test_directive_leaves_content_unchanged_if_when_is_true() {
+		$markup = <<<EOF
+			<wp-show when="context.myblock.open">
+				<div>I should be shown!</div>
+			</wp-show>
+EOF;
+		$tags = new WP_HTML_Processor( $markup );
+		$tags->next_tag();
+
+		$context_before = new WP_Directive_Context( array( 'myblock' => array( 'open' => true ) ) );
+		$context = $context_before;
+		process_wp_show( $tags, $context );
+
+		// TODO:
+		// $content = trim ( $tags->get_content_inside_balanced_tags() );
+		// $this->assertSame( '<div>I should be shown!</div>', $content );
+		$this->assertSame( $context_before->get_context(), $context->get_context(), 'wp-show directive changed context' );
+	}
+
+	public function test_directive_wraps_content_in_template_if_when_is_false() {
+		$markup = <<<EOF
+			<wp-show when="context.myblock.open">
+				<div>I should be shown!</div>
+			</wp-show>
+EOF;
+		$tags = new WP_HTML_Processor( $markup );
+		$tags->next_tag();
+
+		$context_before = new WP_Directive_Context( array( 'myblock' => array( 'open' => false ) ) );
+		$context = $context_before;
+		process_wp_show( $tags, $context );
+
+		// TODO:
+		// $content = trim( $tags->get_content_inside_balanced_tags() );
+		// $this->assertSame( '<template><div>I should be shown!</div></template>', $content );
+		$this->assertSame( $context_before->get_context(), $context->get_context(), 'wp-show directive changed context' );
+	}
+}
