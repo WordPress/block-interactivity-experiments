d43a73ab04577a4ef41f03ef9afa4024c2c756c1
Luis Herranz <luisherranz@gmail.com>
2022-12-27 19:43:56 +0100

    Remove unnecesary utils

--- src/runtime/utils.js
+++ src/runtime/utils.js
@@ -19,36 +19,6 @@ export const createRootFragment = (parent, replaceNode) => {
 	});
 };
 
-// Helper function to await until the CPU is idle.
-export const idle = () =>
-	new Promise((resolve) => window.requestIdleCallback(resolve));
-
-export const knownSymbols = new Set(
-	Object.getOwnPropertyNames(Symbol)
-		.map((key) => Symbol[key])
-		.filter((value) => typeof value === 'symbol')
-);
-const supported = new Set([
-	Object,
-	Array,
-	Int8Array,
-	Uint8Array,
-	Uint8ClampedArray,
-	Int16Array,
-	Uint16Array,
-	Int32Array,
-	Uint32Array,
-	Float32Array,
-	Float64Array,
-]);
-export const shouldWrap = ({ constructor }) => {
-	const isBuiltIn =
-		typeof constructor === 'function' &&
-		constructor.name in globalThis &&
-		globalThis[constructor.name] === constructor;
-	return !isBuiltIn || supported.has(constructor);
-};
-
 // Deep Merge
 const isObject = (item) =>
 	item && typeof item === 'object' && !Array.isArray(item);
