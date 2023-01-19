8e2248d7edb6751fdf5245746fa4109cea674912
Bernie Reiter <ockham@raz.or.at>
2023-01-19 11:28:38 +0100

    Add PHPDoc for WP_Directive_Context

--- src/directives/class-wp-directive-context.php
+++ src/directives/class-wp-directive-context.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ * This is a data structure to hold the current context.
+ *
+ * Whenever encountering a `wp-context` directive, we need to update
+ * the context with the data found in that directive. Conversely,
+ * when "leaving" that context (by encountering a closing tag), we
+ * need to reset the context to its previous state. This means that
+ * we actually need sort of a stack to keep track of all nested contexts.
+ *
+ * Example:
+ *
+ * <wp-context data='{ "foo": 123 }'>
+ *     <!-- foo should be 123 here. -->
+ *     <wp-context data='{ "foo": 456 }'>
+ *         <!-- foo should be 456 here. -->
+ *     </wp-context>
+ *     <!-- foo should be reset to 123 here. -->
+ * </wp-context>
+ */
 class WP_Directive_Context {
 	protected $stack = array( array() );
 
