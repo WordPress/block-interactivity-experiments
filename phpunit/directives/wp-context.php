b63df865a039dc081566661ed721df6fdc1dcfd8
Bernie Reiter <ockham@raz.or.at>
2023-01-19 15:25:58 +0100

    Add more granular unit tests for wp-context

--- phpunit/directives/wp-context.php
+++ phpunit/directives/wp-context.php
@@ -16,6 +16,51 @@ require_once __DIR__ . '/../../../gutenberg/lib/experimental/html/index.php';
  * @covers process_wp_context
  */
 class Tests_Directives_WpContext extends WP_UnitTestCase {
+	public function test_directive_merges_context_correctly_upon_opening_wp_context_tag() {
+		$context = new WP_Directive_Context(
+			array(
+				'myblock'    => array( 'open' => false ),
+				'otherblock' => array( 'somekey' => 'somevalue' )
+			)
+		);
+
+		$markup = '<wp-context data=\'{ "myblock": { "open": true } }\'>';
+		$tags = new WP_HTML_Tag_Processor( $markup );
+		$tags->next_tag();
+
+		process_wp_context( $tags, $context );
+
+		$this->assertSame(
+			array(
+				'myblock'    => array( 'open' => true ),
+				'otherblock' => array( 'somekey' => 'somevalue' ),
+			),
+			$context->get_context()
+		);
+	}
+
+	public function test_directive_resets_context_correctly_upon_closing_wp_context_tag() {
+		$context = new WP_Directive_Context(
+			array( 'my-key' => 'original-value' )
+		);
+
+		$context->set_context(
+			array( 'my-key' => 'new-value' )
+		);
+
+		$markup = '</wp-context>';
+		$tags = new WP_HTML_Tag_Processor( $markup );
+		$tags->next_tag( array( 'tag_closers' => 'visit' ) );
+
+		process_wp_context( $tags, $context );
+
+		$this->assertSame(
+			array( 'my-key' => 'original-value' ),
+			$context->get_context()
+		);
+	}
+
+
 	public function test_directive_updates_context() {
 		$context = new WP_Directive_Context( array( 'myblock' => array( 'open' => false ) ) );
 
