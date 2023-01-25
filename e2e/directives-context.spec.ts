9059d8741175172cea3128ea0b9edffefcaa31d0
David Arenas <david.arenas@automattic.com>
2023-01-25 13:23:19 +0100

    Add e2e spec for context

---
+++ e2e/directives-context.spec.ts
@@ -0,0 +1,142 @@
+import { join } from 'path';
+import { test, expect, Locator } from '@playwright/test';
+
+const parseContent = async (loc: Locator) =>
+	JSON.parse((await loc.textContent()) || '');
+
+test.describe('wp-context', () => {
+	test.beforeEach(async ({ page }) => {
+		await page.goto('file://' + join(__dirname, 'directives-context.html'));
+	});
+
+	test('properties can be inherited', async ({ page }) => {
+		const context = page.getByTestId('child context');
+		const prop1Button = page.getByTestId('parent prop1');
+		const prop4Button = page.getByTestId('parent obj.prop4');
+
+		// Properties from parent should be inheried.
+		{
+			const {
+				prop1,
+				obj: { prop4 },
+			} = await parseContent(context);
+
+			expect(prop1).toBe('parent');
+			expect(prop4).toBe('parent');
+		}
+
+		// Changes in parent context should be reflected.
+		await prop1Button.click();
+		await prop4Button.click();
+
+		{
+			const {
+				prop1,
+				obj: { prop4 },
+			} = await parseContent(context);
+
+			expect(prop1).toBe('modified');
+			expect(prop4).toBe('modified');
+		}
+	});
+
+	test('inherited properties can be modified', async ({ page }) => {
+		const parentContext = page.getByTestId('parent context');
+		const childContext = page.getByTestId('child context');
+		const prop1Button = page.getByTestId('child prop1');
+		const prop4Button = page.getByTestId('child obj.prop4');
+
+		// Properties from parent should be inheried.
+		{
+			const {
+				prop1,
+				obj: { prop4 },
+			} = await parseContent(childContext);
+
+			expect(prop1).toBe('parent');
+			expect(prop4).toBe('parent');
+		}
+
+		// Changes in child context should be reflected in parent.
+		await prop1Button.click();
+		await prop4Button.click();
+
+		{
+			const {
+				prop1,
+				obj: { prop4 },
+			} = await parseContent(parentContext);
+
+			expect(prop1).toBe('modified');
+			expect(prop4).toBe('modified');
+		}
+	});
+
+	test('properties can be shadowed', async ({ page }) => {
+		const parentContext = page.getByTestId('parent context');
+		const childContext = page.getByTestId('child context');
+		const prop1Button = page.getByTestId('child prop2');
+		const prop4Button = page.getByTestId('child obj.prop5');
+
+		// Shadowed properties should NOT be inheried from parent.
+		{
+			const {
+				prop2,
+				obj: { prop5 },
+			} = await parseContent(childContext);
+
+			expect(prop2).toBe('child');
+			expect(prop5).toBe('child');
+		}
+
+		// Shadowed properties should NOT have changed in parent.
+		{
+			const {
+				prop2,
+				obj: { prop5 },
+			} = await parseContent(parentContext);
+
+			expect(prop2).toBe('parent');
+			expect(prop5).toBe('parent');
+		}
+
+		// Changes in shadowed context should NOT affect parent.
+		await prop1Button.click();
+		await prop4Button.click();
+
+		{
+			const {
+				prop2,
+				obj: { prop5 },
+			} = await parseContent(childContext);
+
+			expect(prop2).toBe('modifiedFromChild');
+			expect(prop5).toBe('modifiedFromChild');
+		}
+
+		{
+			const {
+				prop2,
+				obj: { prop5 },
+			} = await parseContent(parentContext);
+
+			expect(prop2).toBe('parent');
+			expect(prop5).toBe('parent');
+		}
+	});
+
+	test('array properties are shadowed', async ({ page }) => {
+		const parentContext = page.getByTestId('parent context');
+		const childContext = page.getByTestId('child context');
+
+		{
+			const { array } = await parseContent(parentContext);
+			expect(array).toMatchObject([1, 2, 3]);
+		}
+
+		{
+			const { array } = await parseContent(childContext);
+			expect(array).toMatchObject([4, 5, 6]);
+		}
+	});
+});
--- e2e/wpx.js
+++ e2e/wpx.js
@@ -5,6 +5,11 @@ wpx({
 		trueValue: true,
 		falseValue: false,
 	},
+	derived: {
+		renderContext: ({ context }) => {
+			return JSON.stringify(context, undefined, 2);
+		},
+	},
 	actions: {
 		toggleTrueValue: ({ state }) => {
 			state.trueValue = !state.trueValue;
@@ -14,6 +19,12 @@ wpx({
 		},
 		toggleContextFalseValue: ({ context }) => {
 			context.falseValue = !context.falseValue;
+		},
+		updateContext: ({ context, event }) => {
+			const { name, value } = event.target;
+			const [key, ...path] = name.split('.').reverse();
+			const obj = path.reduceRight((o, k) => o[k], context);
+			obj[key] = value;
 		},
 	},
 });
