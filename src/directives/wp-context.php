a771378d84364f8dba7fa956d25a49288b1669a8
Bernie Reiter <ockham@raz.or.at>
2023-01-26 14:34:52 +0100

    Remove now-obsolete logic from wp-context tag processor

--- src/directives/wp-context.php
+++ src/directives/wp-context.php
@@ -1,17 +1,12 @@
 <?php
 
 function process_wp_context( $tags, $context ) {
-	if ( 'WP-CONTEXT' === $tags->get_tag() ) {
-		if ( $tags->is_tag_closer() ) {
-			$context->rewind_context();
-			return;
-		}
-		$value = $tags->get_attribute( 'data' );
-	} else {
-		// TODO: Implement rewinding context upon matching closing tag.
-		$value = $tags->get_attribute( 'wp-context' );
+	if ( $tags->is_tag_closer() ) {
+		$context->rewind_context();
+		return;
 	}
 
+	$value = $tags->get_attribute( 'data' );
 	if ( null === $value ) {
 		// No wp-context directive.
 		return;
