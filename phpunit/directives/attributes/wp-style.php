bb6abaa5a4a7a01665b37544aca80636516a4431
Bernie Reiter <ockham@raz.or.at>
2023-01-31 15:53:26 +0100

    SSR: Handle attribute directives separately from tag directives (#140)
    - Create new subfolders `attributes` and `tags` inside both `src/directives/` and `phpunit/directives/`.
    - In `wp-directives.php`, use separate arrays for attribute and tag directives.
    - Remove now-obsolete code and tests from attribute directive processors (that would've dealt with being called on a tag directive).
    - Differentiate `process_wp_context_tag` and `process_wp_context_attribute`.
    - Disable unit test for `wp-context` attribute directive, as it's not implemented yet.

--- phpunit/directives/attributes/wp-style.php
+++ phpunit/directives/attributes/wp-style.php
@@ -3,11 +3,11 @@
  * wp-style directive test.
  */
 
-require_once __DIR__ . '/../../src/directives/wp-style.php';
+require_once __DIR__ . '/../../../src/directives/attributes/wp-style.php';
 
-require_once __DIR__ . '/../../src/directives/class-wp-directive-context.php';
+require_once __DIR__ . '/../../../src/directives/class-wp-directive-context.php';
 
-require_once __DIR__ . '/../../../gutenberg/lib/experimental/html/index.php';
+require_once __DIR__ . '/../../../../gutenberg/lib/experimental/html/index.php';
 
 /**
  * Tests for the wp-style directive.
@@ -35,20 +35,6 @@ class Tests_Directives_WpStyle extends WP_UnitTestCase {
 
 	public function test_directive_ignores_empty_style() {
 		$markup = '<div wp-style:="context.myblock.color" style="background: blue;">Test</div>';
-		$tags = new WP_HTML_Tag_Processor( $markup );
-		$tags->next_tag();
-
-		$context_before = new WP_Directive_Context( array( 'myblock' => array( 'color' => 'green' ) ) );
-		$context = $context_before;
-		process_wp_style( $tags, $context );
-
-		$this->assertSame( $markup, $tags->get_updated_html() );
-		$this->assertStringNotContainsString( 'color: green;', $tags->get_attribute( 'style' ) );
-		$this->assertSame( $context_before->get_context(), $context->get_context(), 'wp-style directive changed context' );
-	}
-
-	public function test_directive_ignores_wp_style_tag() {
-		$markup = '<wp-style:color="context.myblock.color" style="background: blue;">Test</div>';
 		$tags = new WP_HTML_Tag_Processor( $markup );
 		$tags->next_tag();
 
