b6e4d12fe684e8d10fa94f9707a0597da1d6b645
Bernie Reiter <ockham@raz.or.at>
2023-02-01 11:14:55 +0100

    Remove duplicated wp-context attribute directive test

--- phpunit/directives/tags/wp-context.php
+++ phpunit/directives/tags/wp-context.php
@@ -59,29 +59,4 @@ class Tests_Directives_Tags_WpContext extends WP_UnitTestCase {
 			$context->get_context()
 		);
 	}
-
-	public function test_directive_merges_context_correctly_upon_wp_context_attribute_on_opening_tag() {
-		$this->markTestSkipped( 'Need to implement the wp-context attribute directive processor first.' );
-
-		$context = new WP_Directive_Context(
-			array(
-				'myblock'    => array( 'open' => false ),
-				'otherblock' => array( 'somekey' => 'somevalue' ),
-			)
-		);
-
-		$markup = '<div wp-context=\'{ "myblock": { "open": true } }\'>';
-		$tags = new WP_HTML_Tag_Processor( $markup );
-		$tags->next_tag();
-
-		process_wp_context_tag( $tags, $context );
-
-		$this->assertSame(
-			array(
-				'myblock'    => array( 'open' => true ),
-				'otherblock' => array( 'somekey' => 'somevalue' ),
-			),
-			$context->get_context()
-		);
-	}
 }
