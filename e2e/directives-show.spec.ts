374e810f73163cf052400bf6f74c656d36690444
Mario Santos <santosguillamot@gmail.com>
2023-02-22 13:42:18 +0100

    Add more tests to `wp-show` directive attribute

--- e2e/directives-show.spec.ts
+++ e2e/directives-show.spec.ts
@@ -6,10 +6,41 @@ test.describe('wp-show', () => {
 		await page.goto('file://' + join(__dirname, 'directives-show.html'));
 	});
 
+	test('show if callback returns truthy value', async ({ page }) => {
+		await page.pause();
+		const el = page.getByTestId('show if callback returns truthy value');
+		await expect(el).toBeVisible();
+	});
+
 	test('do not show if callback returns falsy value', async ({ page }) => {
-		const el1 = page.getByTestId('show if callback returns truthy value');
-		await expect(el1).toBeVisible();
-		const el2 = page.getByTestId('do not show if callback returns false value')
-		await expect(el2).toBeHidden();
+		const el = page.getByTestId('do not show if callback returns false value')
+		await expect(el).toBeHidden();
+	});
+
+	test('hide when toggling truthy value to falsy', async ({ page }) => {
+		const el = page.getByTestId('show if callback returns truthy value');
+		await expect(el).toBeVisible();
+		page.getByTestId('toggle trueValue').click();
+		await expect(el).toBeHidden();
+		page.getByTestId('toggle trueValue').click();
+		await expect(el).toBeVisible();
+	});
+
+	test('show when toggling false value to truthy', async ({ page }) => {
+		const el = page.getByTestId('do not show if callback returns false value');
+		await expect(el).toBeHidden();
+		page.getByTestId('toggle falseValue').click();
+		await expect(el).toBeVisible();
+		page.getByTestId('toggle falseValue').click();
+		await expect(el).toBeHidden();
+	});
+
+	test('can use context values', async ({ page }) => {
+		const el = page.getByTestId('can use context values');
+		await expect(el).toBeHidden();
+		page.getByTestId('toggle context false value').click();
+		await expect(el).toBeVisible();
+		page.getByTestId('toggle context false value').click();
+		await expect(el).toBeHidden();
 	});
 });
