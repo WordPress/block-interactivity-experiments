a75e8bbe790b083de70992950391fdfb12fc8f49
David Arenas <david.arenas@automattic.com>
2023-02-24 17:52:47 +0100

    Add failing e2e test

--- e2e/tovdom-full.spec.ts
+++ e2e/tovdom-full.spec.ts
@@ -3,10 +3,26 @@ import { test, expect } from '@playwright/test';
 
 test.describe('toVdom - full', () => {
 	test.beforeEach(async ({ page }) => {
-		await page.goto('file://' + join(__dirname, 'tovdom-full.html'));
+		await page.route('**/*.html', async (route, req) => {
+			const { pathname } = new URL(req.url());
+			route.fulfill({ path: join(__dirname, pathname) });
+		});
+
+		await page.goto('http://a.b/tovdom-full.html');
 	});
 
 	test('it should stop when it founds wp-ignore', async ({ page }) => {
+		const el = page.getByTestId('inside wp-ignore');
+		await expect(el).toBeVisible();
+	});
+
+	test('it should not change wp-ignore content after navigation', async ({
+		page,
+	}) => {
+		// Next HTML purposely removes all content inside `wp-ignore`.
+		await page.getByRole('link').click();
+
+		// HTML content inside `wp-ignore` should remain.
 		const el = page.getByTestId('inside wp-ignore');
 		await expect(el).toBeVisible();
 	});
