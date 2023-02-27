039f289c0d7d581e808479687dd4e4c8bbd00052
David Arenas <david.arenas@automattic.com>
2023-02-27 17:50:20 +0100

    Correct added e2e test

--- e2e/tovdom-full.spec.ts
+++ e2e/tovdom-full.spec.ts
@@ -3,9 +3,15 @@ import { test, expect } from '@playwright/test';
 
 test.describe('toVdom - full', () => {
 	test.beforeEach(async ({ page }) => {
+		// Helpers to use URLs with http:// instead of file:// to avoid errors
+		// inside `fetch` calls.
 		await page.route('**/*.html', async (route, req) => {
 			const { pathname } = new URL(req.url());
 			route.fulfill({ path: join(__dirname, pathname) });
+		});
+		await page.route('**/*.js', async (route, req) => {
+			const { pathname } = new URL(req.url());
+			route.fulfill({ path: join(__dirname, '..', pathname) });
 		});
 
 		await page.goto('http://a.b/tovdom-full.html');
@@ -19,11 +25,16 @@ test.describe('toVdom - full', () => {
 	test('it should not change wp-ignore content after navigation', async ({
 		page,
 	}) => {
-		// Next HTML purposely removes all content inside `wp-ignore`.
-		await page.getByRole('link').click();
+		// Next HTML purposely changes content inside `wp-ignore`.
+		await page.getByTestId('next').click();
 
-		// HTML content inside `wp-ignore` should remain.
-		const el = page.getByTestId('inside wp-ignore');
-		await expect(el).toBeVisible();
+		const oldContent = page.getByTestId('inside wp-ignore');
+		await expect(oldContent).toBeVisible();
+
+		const newContent = page.getByTestId('new content inside wp-ignore');
+		await expect(newContent).not.toBeVisible();
+
+		const link = page.getByTestId('next');
+		await expect(link).not.toBeVisible();
 	});
 });
