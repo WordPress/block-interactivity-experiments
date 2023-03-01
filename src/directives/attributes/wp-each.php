520ff9215564b1fa469b16f5b19eab0f28953277
Bernie Reiter <ockham@raz.or.at>
2023-03-01 19:04:11 +0100

    Add pseudo-code-ish wp-each implementation

---
+++ src/directives/attributes/wp-each.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once __DIR__ . '/../class-wp-directive-processor.php';
+
+function process_wp_each( $tags, $context ) {
+	if ( $tags->is_tag_closer() ) {
+		return;
+	}
+
+	$prefixed_attributes = $tags->get_attribute_names_with_prefix( 'wp-each:' );
+	if ( 0 === count( $prefixed_attributes ) ) {
+		return;
+	}
+	$attribute_name = $prefixed_attributes[0];
+
+	list( , $iterator_name ) = explode( ':', $attribute_name );
+
+	$value = $tags->get_attribute( $attribute_name );
+	if ( null === $value ) {
+		// No wp-each directive.
+		return;
+	}
+
+	$loop_array = evaluate( $value, $context->get_context() );
+	// TODO: Error handling.
+
+	$loop_inner_html = '';
+	foreach ( $loop_array as $iteration_item ) {
+		$context->set_context( array( 'item' => $iteration_item ) );
+
+		$inner_html       = $tags->get_inner_html();
+		$inner_tags       = new WP_Directive_Processor( $inner_html );
+		$inner_tags       = wp_process_directives( $inner_tags, $context );
+		$loop_inner_html .= $inner_tags->get_inner_html();
+
+		$context->rewind_context();
+	}
+	$tags->set_inner_html( $loop_inner_html );
+	$tags->find_balanced_tag();
+}
