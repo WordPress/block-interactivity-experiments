f246af90ee6f4cbd9d1c470b10def5b3d47463fd
Mario Santos <santosguillamot@gmail.com>
2023-03-09 10:52:58 +0100

    Revert "Bump plugin version to 0.1.1"
    This reverts commit 35554f7d85f510fa7a0b85ea7400d03a1c1c573e.

--- wp-directives.php
+++ wp-directives.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name:       wp-directives
- * Version:           0.1.1
+ * Version:           0.1.0
  * Requires at least: 6.0
  * Requires PHP:      5.6
  * Author:            Gutenberg Team
