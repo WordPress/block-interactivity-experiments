351635ece27be4a8aa69f71bd7921902267cf8fa
Bernie Reiter <ockham@raz.or.at>
2023-03-14 13:39:34 +0100

    Add PHPDoc

--- src/directives/class-wp-directive-processor.php
+++ src/directives/class-wp-directive-processor.php
@@ -35,6 +35,16 @@ class WP_Directive_Processor extends WP_HTML_Tag_Processor {
 		return false;
 	}
 
+	/**
+	 * Find the matching closing tag for an opening tag.
+	 *
+	 * When called while on an open tag, traverse the HTML until we find
+	 * the matching closing tag, respecting any in-between content, including
+	 * nested tags of the same name. Return false when called on a closing or
+	 * void tag, or if no matching closing tag was found.
+	 *
+	 * @return bool True if a matching closing tag was found.
+	 */
 	public function next_balanced_closer() {
 		$depth = 0;
 
@@ -60,6 +70,14 @@ class WP_Directive_Processor extends WP_HTML_Tag_Processor {
 		return false;
 	}
 
+	/**
+	 * Return the content between two balanced tags.
+	 *
+	 * When called on an opening tag, return the HTML content found between
+	 * that opening tag and its matching closing tag.
+	 *
+	 * @return string The content between the current opening and its matching closing tag.
+	 */
 	public function get_inner_html() {
 		$this->set_bookmark( 'start' );
 		if ( ! $this->next_balanced_closer() ) {
@@ -78,6 +96,15 @@ class WP_Directive_Processor extends WP_HTML_Tag_Processor {
 		return substr( $this->html, $start, $end - $start );
 	}
 
+	/**
+	 * Set the content between two balanced tags.
+	 *
+	 * When called on an opening tag, set the HTML content found between
+	 * that opening tag and its matching closing tag.
+	 *
+	 * @param string $new_html The string to replace the content between the matching tags with.
+	 * @return bool            Whether the content was successfully replaced.
+	 */
 	public function set_inner_html( $new_html ) {
 		$this->get_updated_html(); // Apply potential previous updates.
 
@@ -99,6 +126,14 @@ class WP_Directive_Processor extends WP_HTML_Tag_Processor {
 		return true;
 	}
 
+	/**
+	 * Whether a given HTML element is void (e.g. <br>).
+	 *
+	 * @param string $tag_name The element in question.
+	 * @return bool True if the element is void.
+	 *
+	 * @see https://html.spec.whatwg.org/#elements-2
+	 */
 	public static function is_html_void_element( $tag_name ) {
 		switch ( $tag_name ) {
 			case 'AREA':
