71beac0f6f4ac39d15fe9d5b74a8b658a7bb54bb
Mario Santos <santosguillamot@gmail.com>
2023-03-16 13:57:11 +0100

    Change unit tests

--- phpunit/directives/attributes/wp-style.php
+++ phpunit/directives/attributes/wp-style.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * wp-style directive test.
+ * data-wp-style directive test.
  */
 
 require_once __DIR__ . '/../../../src/directives/attributes/wp-style.php';
@@ -10,14 +10,14 @@ require_once __DIR__ . '/../../../src/directives/class-wp-directive-context.php'
 require_once __DIR__ . '/../../../src/directives/wp-html.php';
 
 /**
- * Tests for the wp-style directive.
+ * Tests for the data-wp-style directive.
  *
  * @group  directives
  * @covers process_wp_style
  */
 class Tests_Directives_WpStyle extends WP_UnitTestCase {
 	public function test_directive_adds_style() {
-		$markup = '<div wp-style:color="context.myblock.color" style="background: blue;">Test</div>';
+		$markup = '<div data-wp-style:color="context.myblock.color" style="background: blue;">Test</div>';
 		$tags   = new WP_HTML_Tag_Processor( $markup );
 		$tags->next_tag();
 
@@ -26,15 +26,15 @@ class Tests_Directives_WpStyle extends WP_UnitTestCase {
 		process_wp_style( $tags, $context );
 
 		$this->assertSame(
-			'<div wp-style:color="context.myblock.color" style="background: blue;color: green;">Test</div>',
+			'<div data-wp-style:color="context.myblock.color" style="background: blue;color: green;">Test</div>',
 			$tags->get_updated_html()
 		);
 		$this->assertStringContainsString( 'color: green;', $tags->get_attribute( 'style' ) );
-		$this->assertSame( $context_before->get_context(), $context->get_context(), 'wp-style directive changed context' );
+		$this->assertSame( $context_before->get_context(), $context->get_context(), 'data-wp-style directive changed context' );
 	}
 
 	public function test_directive_ignores_empty_style() {
-		$markup = '<div wp-style:="context.myblock.color" style="background: blue;">Test</div>';
+		$markup = '<div data-wp-style:="context.myblock.color" style="background: blue;">Test</div>';
 		$tags   = new WP_HTML_Tag_Processor( $markup );
 		$tags->next_tag();
 
@@ -44,6 +44,6 @@ class Tests_Directives_WpStyle extends WP_UnitTestCase {
 
 		$this->assertSame( $markup, $tags->get_updated_html() );
 		$this->assertStringNotContainsString( 'color: green;', $tags->get_attribute( 'style' ) );
-		$this->assertSame( $context_before->get_context(), $context->get_context(), 'wp-style directive changed context' );
+		$this->assertSame( $context_before->get_context(), $context->get_context(), 'data-wp-style directive changed context' );
 	}
 }
