bb7bff3a7f4965faab975f6a23dbb1fc5254a5d3
Weston Ruter <westonruter@google.com>
2023-03-29 15:58:12 -0700

    Fix issues in admin-page.php

--- src/admin/admin-page.php
+++ src/admin/admin-page.php
@@ -1,9 +1,17 @@
 <?php
+/**
+ * Functions for admin page.
+ *
+ * @package block-interactivity-experiments
+ */
 
+/**
+ * Register admin menu.
+ */
 function wp_directives_register_menu() {
 	add_options_page(
-		'WP Directives',
-		'WP Directives',
+		__( 'WP Directives', 'wp-directives' ),
+		__( 'WP Directives', 'wp-directives' ),
 		'manage_options',
 		'wp-directives-plugin',
 		'wp_directives_render_admin_page'
@@ -11,20 +19,27 @@ function wp_directives_register_menu() {
 }
 add_action( 'admin_menu', 'wp_directives_register_menu' );
 
-function wp_directives_render_admin_page() {    ?>
+/**
+ * Render admin page.
+ */
+function wp_directives_render_admin_page() {
+	?>
 	<div class="wrap">
-	  <h2>WP Directives</h2>
-	  <form method="POST" action="options.php">
-		  <?php
+		<h2>WP Directives</h2>
+		<form method="POST" action="options.php">
+			<?php
 			settings_fields( 'wp_directives_plugin_settings' );
 			do_settings_sections( 'wp_directives_plugin_page' );
 			?>
-		  <?php submit_button(); ?>
-	  </form>
+			<?php submit_button(); ?>
+		</form>
 	</div>
 	<?php
 }
 
+/**
+ * Register settings.
+ */
 function wp_directives_register_settings() {
 	register_setting(
 		'wp_directives_plugin_settings',
@@ -41,13 +56,13 @@ function wp_directives_register_settings() {
 	add_settings_section(
 		'wp_directives_plugin_section',
 		'',
-		null,
+		null, // TODO: This is supposed to be a callable.
 		'wp_directives_plugin_page'
 	);
 
 	add_settings_field(
 		'client_side_navigation',
-		'Client Side Navigation',
+		__( 'Client Side Navigation', 'wp-directives' ),
 		'wp_directives_client_side_navigation_input',
 		'wp_directives_plugin_page',
 		'wp_directives_plugin_section'
@@ -55,20 +70,28 @@ function wp_directives_register_settings() {
 }
 add_action( 'admin_init', 'wp_directives_register_settings' );
 
+/**
+ * Validate settings.
+ *
+ * @param array $input Unvalidated setting.
+ * @return array Validated setting.
+ */
 function wp_directives_validate_settings( $input ) {
 	$output                           = get_option( 'wp_directives_plugin_settings' );
-	$output['client_side_navigation'] =
-		isset( $input ) && $input['client_side_navigation'] ? true : false;
+	$output['client_side_navigation'] = ! empty( $input['client_side_navigation'] );
 	return $output;
 }
 
+/**
+ * Render field for client-side navigation.
+ */
 function wp_directives_client_side_navigation_input() {
 	$options = get_option( 'wp_directives_plugin_settings' );
 	?>
 
-	<input type="checkbox" 
-		name="<?php echo esc_attr( 'wp_directives_plugin_settings[client_side_navigation]' ); ?>" 
-		<?php echo $options['client_side_navigation'] ? 'checked' : ''; ?>
+	<input type="checkbox"
+		name="wp_directives_plugin_settings[client_side_navigation]"
+		<?php checked( $options['client_side_navigation'] ); ?>
 	>
 
 	<?php
