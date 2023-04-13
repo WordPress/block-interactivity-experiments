d2e68d3d44db34cc988eea00de8479f58503994d
Bernie Reiter <ockham@raz.or.at>
2023-04-13 10:24:01 +0200

    Increase test coverage -- new test currently fails

--- phpunit/directives/attributes/wp-show.php
+++ phpunit/directives/attributes/wp-show.php
@@ -52,6 +52,25 @@ class Tests_Directives_WpShow extends WP_UnitTestCase {
 		$this->assertSame( $context_before->get_context(), $context->get_context(), 'data-wp-show directive changed context' );
 	}
 
+	public function test_directive_wraps_content_preceded_by_other_html_in_template_if_when_is_false() {
+		$markup = '<p>Some text</p><div data-wp-show="context.myblock.open">I should not be shown!</div>';
+
+		$tags = new WP_Directive_Processor( $markup );
+		$tags->next_tag( 'div' );
+
+		$context_before = new WP_Directive_Context( array( 'myblock' => array( 'open' => false ) ) );
+		$context        = clone $context_before;
+		process_wp_show( $tags, $context );
+
+		$tags->next_tag( array( 'tag_closers' => 'visit' ) );
+		process_wp_show( $tags, $context );
+
+		$updated_markup = '<p>Some text</p><template data-wp-show="context.myblock.open"><div >I should not be shown!</div></template>';
+
+		$this->assertSame( $updated_markup, $tags->get_updated_html() );
+		$this->assertSame( $context_before->get_context(), $context->get_context(), 'data-wp-show directive changed context' );
+	}
+
 	public function test_directive_wraps_void_tag_in_template_if_when_is_false() {
 		$markup = '<img data-wp-show="context.myblock.open">';
 		$tags   = new WP_Directive_Processor( $markup );
