ef1088bc03910ade6bf0aa948483b4263171960f
David Arenas <david.arenas@automattic.com>
2023-04-18 19:00:58 +0200

    Remove confusing comment
    Co-authored-by: Luis Herranz <luisherranz@gmail.com>

--- src/runtime/hydration.js
+++ src/runtime/hydration.js
@@ -3,7 +3,6 @@ import { toVdom, hydratedIslands } from './vdom';
 import { createRootFragment } from './utils';
 import { directivePrefix } from './constants';
 
-// Initialize the router with the initial DOM.
 export const init = async () => {
 	document.querySelectorAll(`[${directivePrefix}island]`).forEach((node) => {
 		if (!hydratedIslands.has(node)) {
