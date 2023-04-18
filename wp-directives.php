d9e5271e84d9cd060f4fd7ee04e7ec161bc3f19d
Bernie Reiter <ockham@raz.or.at>
2023-04-18 14:39:01 +0200

    Revert "Don't process directives in inner blocks"
    This reverts commit 16faac9f07967020b42267f4a85d36cae36fdb2c.

--- wp-directives.php
+++ wp-directives.php
@@ -234,25 +234,20 @@ add_filter( 'render_block', 'wp_directives_mark_interactive_blocks', 10, 3 );
  */
 function wp_directives_inner_blocks( $parsed_block, $source_block, $parent_block ) {
 	if (
-		isset( $parent_block )
-	) {
-		if (
-			block_has_support(
-				$parent_block->block_type,
-				array(
-					'interactivity',
-					'isolated',
-				)
+		isset( $parent_block ) &&
+		block_has_support(
+			$parent_block->block_type,
+			array(
+				'interactivity',
+				'isolated',
 			)
-		) {
-			$parsed_block['isolated'] = true;
-		}
-		$parsed_block['inner_block'] = true; // TODO: Limit to interactive blocks.
+		)
+	) {
+		$parsed_block['isolated'] = true;
 	}
 	return $parsed_block;
 }
 add_filter( 'render_block_data', 'wp_directives_inner_blocks', 10, 3 );
-
 
 /**
  * Process directives in block.
@@ -260,11 +255,7 @@ add_filter( 'render_block_data', 'wp_directives_inner_blocks', 10, 3 );
  * @param string $block_content Block content.
  * @return string Filtered block content.
  */
-function process_directives_in_block( $block_content, $block, $instance ) {
-	if ( isset( $instance->parsed_block['inner_block'] ) ) {
-		return $block_content;
-	}
-
+function process_directives_in_block( $block_content ) {
 	// TODO: Add some directive/components registration mechanism.
 	$directives = array(
 		'data-wp-context' => 'process_wp_context',
@@ -284,7 +275,7 @@ add_filter(
 	'render_block',
 	'process_directives_in_block',
 	10,
-	3
+	1
 );
 
 // TODO: check if priority 9 is enough.
