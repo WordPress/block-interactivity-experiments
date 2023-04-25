39ccb1898d7df53b324c560a5a1c810f2cca498e
Bernie Reiter <ockham@raz.or.at>
2023-04-25 16:34:14 +0200

    Rewrite wp-show processor

--- src/directives/attributes/wp-show.php
+++ src/directives/attributes/wp-show.php
@@ -13,11 +13,13 @@ function process_wp_show( $tags, $context ) {
 	}
 
 	$show = evaluate( $value, $context->get_context() );
-	if ( ! $show ) {
-		$wrapper_bookmark = $tags->wrap_in_tag( 'TEMPLATE' );
-		$tags->seek( $wrapper_bookmark );
-		$tags->set_attribute( 'data-wp-show', $value );
-		$tags->next_tag();
-		$tags->remove_attribute( 'data-wp-show' );
+	if ( $show ) {
+		return;
 	}
+
+	$wrapper_bookmark = $tags->wrap_in_tag( 'TEMPLATE' );
+	$tags->seek( $wrapper_bookmark );
+	$tags->set_attribute( 'data-wp-show', $value );
+	$tags->next_tag();
+	$tags->remove_attribute( 'data-wp-show' );
 }
